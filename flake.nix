c447537200be2f5ecd08bcd03e41a6da95ecb34d
SnO2WMaN <me@sno2wman.net>
2022-11-18 19:41:59 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  # main
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = with inputs; [
+            devshell.overlay
+          ];
+        };
+      in {
+        devShells.default = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            alejandra
+            treefmt
+            deno
+          ];
+        };
+      }
+    );
+}
